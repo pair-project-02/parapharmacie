18f8cf590f669b2c45ab96e887ee4a0503b952ba
maryemabdelmoumen <abdelmoumenmaryem5@gmail.com>
2024-01-12 00:14:21 +0100

    i created a function that stored data using jQuery but still not working for now

---
+++ step1,login/login.js
@@ -0,0 +1,14 @@
+        $(document).ready(function() {
+            $("#loginBtn").click(function() {
+                var username = $("#username").val();
+                var password = $("#password").val();
+                var rememberMe = $("#rememberMe").prop("checked");
+
+                // Store data in local storage
+                localStorage.setItem("username", username); 
+                console.log(username)
+                localStorage.setItem("password", password);
+                localStorage.setItem("rememberMe", rememberMe);
+                window.location.href = "welcome.html";
+            });
+        });
